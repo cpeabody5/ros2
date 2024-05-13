d04335dac83feb66cff9a27e8fdf30a5c8f88cee
cpeabody5 <coady.peabody@gmail.com>
2024-05-12 23:54:20 -0500

    more stuff

--- ros2_ws/src/my_robot_controller/setup.py
+++ ros2_ws/src/my_robot_controller/setup.py
@@ -21,7 +21,9 @@ setup(
     entry_points={
         'console_scripts': [
             "test_node = my_robot_controller.my_first_node:main",
-            "draw_circle = my_robot_controller.draw_circle:main"
+            "draw_circle = my_robot_controller.draw_circle:main",
+            "pose_subscriber = my_robot_controller.pose_subscriber:main",
+            "turtle_controller = my_robot_controller.turtle_controller:main",
         ],
     },
 )
